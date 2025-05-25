5e5a9fbce3e66cb836f6c35970ce54bfd3a820dc
Karim <kimomomo@hotmail.com>
2025-05-25 13:26:18 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to SART</h1>
+      <p>This is a minimalist starter app.</p>
+    </div>
+  );
+}
+
+export default App;
